082ab387dae220f244babc64aa452fded0d8ab5c
Miriho6643 <mianhoat@gmail.com>
2025-12-12 22:16:25 +0100

    Create main.py

---
+++ software/main.py
@@ -0,0 +1,20 @@
+import time
+import RPi.GPIO as GPIO
+STEP_PIN = 17
+DIR_PIN = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(STEP_PIN,GPIO.OUT)
+GPIO.setup(DIR_PIN,GPIO.OUT)
+def move_cell(steps,direction):
+    GPIO.output(DIR_PIN,direction)
+    for _ in range(steps):
+        GPIO.output(STEP_PIN,True)
+        time.sleep(0.001)
+        GPIO.output(STEP_PIN,False)
+        time.sleep(0.001)
+try:
+    move_cell(800,True)
+    time.sleep(1)
+    move_cell(800,False)
+finally:
+    GPIO.cleanup()
