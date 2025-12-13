500e19a194740e37198f2078312bf4a8338a67e5
Miriho6643 <mianhoat@gmail.com>
2025-12-13 14:09:19 +0100

    Update config.py

--- software/config.py
+++ software/config.py
@@ -4,3 +4,10 @@ GRID_COLS = 4        # Anzahl Spalten
 CELL_WIDTH = 42      # mm pro Grid-Zelle (Breite)
 CELL_HEIGHT = 42     # mm pro Grid-Zelle (Tiefe)
 Z_HEIGHT = 50        # max Höhe Z-Lift
+
+STEPX_PIN = 17
+DIRX_PIN = 27
+STEPY_PIN = 16
+DIRY_PIN = 26
+GRIPPER_PIN = 15
+ZAXE_PIN = 25
