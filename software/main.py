0993eb6c4eadbcbb00a49708607ffc0997b6a1f9
Miriho6643 <mianhoat@gmail.com>
2025-12-15 13:51:46 +0100

    Update main.py

--- software/main.py
+++ software/main.py
@@ -1,1 +1,20 @@
 from controller import *
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/send", methods=["POST"])
+def receive_data():
+    data = request.json["message"]
+    print("Von HTML erhalten:", data)
+
+    antwort = f"Python hat erhalten: {data}"
+    return jsonify({"response": antwort})
+
+if __name__ == "__main__":
+    app.run(debug=True)
